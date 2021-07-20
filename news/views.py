12d177bf6f410ff7dbbc220f46221ca020af28cc
Hastur <ikhripkovskii@gmail.com>
2021-07-20 10:51:56 +0500

    попытка добавить форму добовления новостей

--- news/views.py
+++ news/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 
-
 from .models import News, Category
 
 
@@ -8,7 +7,7 @@ def index(request):
     news = News.objects.all()
     context = {
         'news': news,
-        'title': 'Список новостей',#krya
+        'title': 'Список новостей',  # krya
     }
     return render(request, template_name='news/index.html', context=context)
 
@@ -18,7 +17,12 @@ def get_category(request, category_id):
     category = Category.objects.get(pk=category_id)
     return render(request, 'news/category.html', {'news': news, 'category': category})
 
-def view_news(request,news_id):
-    #news_item =News.objects.get(pk=news_id)
-    news_item=get_object_or_404(News,pk=news_id)
-    return render(request,'news/view_news.html',{"news_item":news_item})
+
+def view_news(request, news_id):
+    # news_item =News.objects.get(pk=news_id)
+    news_item = get_object_or_404(News, pk=news_id)
+    return render(request, 'news/view_news.html', {"news_item": news_item})
+
+
+def add_news(request):
+    return render(request, '/news/add_news.html')
